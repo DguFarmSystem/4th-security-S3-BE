b0d332dcd3d800c8d148ac94789eebeb683be175
박수현 <bagsuhyeon@bagsuhyeon-ui-MacBookAir.local>
2025-05-14 13:20:33 +0900

    First commit

---
+++ src/test/java/farmsystem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package farmsystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
