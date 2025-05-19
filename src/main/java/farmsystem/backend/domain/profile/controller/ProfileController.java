8f603c7c5deeb767048578a889a4ded9a35dde27
Jinsoo Yoon <floreo1242@gmail.com>
2025-05-19 14:33:49 +0900

    [feat] #2 프로필 API 인터페이스 및 구현 추가

--- src/main/java/farmsystem/backend/domain/profile/controller/ProfileController.java
+++ src/main/java/farmsystem/backend/domain/profile/controller/ProfileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/profiles")
-public class ProfileController {
+public class ProfileController implements ProfileApi {
 
     private final ProfileService profileService;
 
