4605d32b66d635990fa9d4def3b107f0c78b77dc
clapppp <ssantta999@gmail.com>
2025-05-21 03:22:39 +0900

    [feat] #4 멤버 엔티티 작성

--- src/main/java/farmsystem/backend/domain/member/entity/Member.java
+++ src/main/java/farmsystem/backend/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import farmsystem.backend.domain.common.entity.BaseTimeEntity;
 import farmsystem.backend.domain.profile.entity.Profile;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,17 +18,32 @@ public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(length = 50, nullable = false)
     private String username;
 
-    @Column(nullable = false)
-    private String password;
+    @Column(length = 15, nullable = false)
+    private String phoneNumb;
 
     @Column(length = 100, nullable = false)
     private String email;
 
-    @OneToMany(mappedBy = "member")
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public Member(String username, String phoneNumb, String email, String password) {
+        this.username = username;
+        this.phoneNumb = phoneNumb;
+        this.email = email;
+        this.password = password;
+    }
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Profile> profiles = new ArrayList<>();
+
+    public void addProfile(Profile profile) {
+        profiles.add(profile);
+    }
 }
