db12a44a25eb0f24d8e72b89e36e04549bef8035
Suhyeon Park <ssantta999@gmail.com>
2025-05-22 17:35:03 +0900

    [remove] #4 불필요한 import문 제거

--- src/main/java/farmsystem/backend/global/config/SecurityConfig.java
+++ src/main/java/farmsystem/backend/global/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package farmsystem.backend.global.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
