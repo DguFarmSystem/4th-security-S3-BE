5c847178827f06009f4c124098d996846bffeba4
jjin70 <jjin0434@naver.com>
2025-05-23 22:28:08 +0900

    [fix] #5 PROFILE_NOT_FOUND 에러코드 오타 수정

--- src/main/java/farmsystem/backend/global/error/ErrorCode.java
+++ src/main/java/farmsystem/backend/global/error/ErrorCode.java
@@ -48,7 +48,7 @@ public enum ErrorCode {
     INVALID_PROFILE_TYPE(HttpStatus.BAD_REQUEST, "T003", "LIVE 프로필입니다."),
 
     // Profile
-    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "S001", "해당 프로필을 찾을 수 없습니다."),
+    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "해당 프로필을 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
